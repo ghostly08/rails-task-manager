321ed79fd9f34084c1771f6034ebdd357ed5fcbe
ghostly08 <steven.produccionbcn@gmail.com>
2022-02-15 23:05:07 +0100

    crud update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,9 +19,23 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_update_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def task_update_params
     params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update", as: :edit
 end
